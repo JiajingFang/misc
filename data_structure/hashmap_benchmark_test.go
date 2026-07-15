fa43244db41ac5688c82fdfc6f75f5c4a77981f2
JiajingFang <jiajing.fang@deliveryhero.com>
2026-07-15 13:53:32 +0200

    chore: add test for realHm

--- data_structure/hashmap_benchmark_test.go
+++ data_structure/hashmap_benchmark_test.go
@@ -1,7 +1,6 @@
 package data_structure
 
 import (
-	"fmt"
 	"math/rand"
 	"slices"
 	"strconv"
@@ -13,27 +12,38 @@ var num = 1000
 
 //https://blog.logrocket.com/benchmarking-golang-improve-function-performance/
 func BenchmarkHashmap(b *testing.B) {
-	size := 1000
+	size := 1000000
 	hm := NewHashMap(&size)
 	for j:= 0; j < size; j++ {
 		//put 1000 keyValue pairs into Hashmap
 		value := strconv.Itoa(rand.Intn(1000) + 1)
 		hm.Set(strconv.Itoa(j), value)
 	}
-	fmt.Println("hash map benchmark results for get value")
     for i := 0; i < b.N; i++ {
 		hm.Get(strconv.Itoa(rand.Intn(1000)))
     }
 }
 
+func BenchmarkRealHashmap(b *testing.B) {
+	size := 1000000
+	hm := make(map[string]string, size)
+	for j:= 0; j < size; j++ {
+		//put 1000 keyValue pairs into Hashmap
+		value := strconv.Itoa(rand.Intn(1000) + 1)
+		hm[strconv.Itoa(j)] = value
+	}
+    for i := 0; i < b.N; i++ {
+		_ = hm[strconv.Itoa(rand.Intn(1000))]
+    }
+}
+
 func BenchmarkSlices(b *testing.B) {
-	size := 1000
+	size := 1000000
 	numbers := []string{}
 	for j:= 0; j < size; j++ {
 		//put 1000 elements into slice
 		numbers = append(numbers, strconv.Itoa(rand.Intn(1000) + 1))
 	}
-	fmt.Println("slices benchmark results for get value")
     for i := 0; i < b.N; i++ {
 		_ = slices.Index(numbers, strconv.Itoa(rand.Intn(1000)))
     }
